5e769ed295ed148aaf41dc08e43443a186c75191
Bohusla <124197054+Bohusla@users.noreply.github.com>
2023-03-16 00:08:09 +0100

    Update Main.java

--- task9/Main.java
+++ task9/Main.java
@@ -17,6 +17,6 @@ public class Main {
         System.out.println("masiv");
         System.out.println(Arrays.toString(array));
         System.out.println("sort masiv");
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array)); //  не получилось запустить метод
     }
 }
